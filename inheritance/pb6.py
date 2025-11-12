dfbf238c184934782e2bf373b1a8a41880bf1f67
Danish Raina <danishraina768@gmail.com>
2025-11-12 12:48:17 +0000

    Solved pb6 and 7

---
+++ inheritance/pb6.py
@@ -0,0 +1,20 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def get_area(self):
+        return self.length * self.width
+
+    def get_perimeter(self):
+        return 2 * (self.length + self.width)
+
+
+class Square(Rectangle):
+    def __init__(self, length):
+        super().__init__(length, length)
+
+sq = Square(4)
+
+print(sq.get_area())
+print(sq.get_perimeter())
